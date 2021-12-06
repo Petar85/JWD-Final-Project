f7dc883e8523cc1f3c213a26b72e5d2dbd127ff7
Ben <92470875+BenjKa@users.noreply.github.com>
2021-12-06 14:41:21 -0800

    added tasks into js

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,7 +1,28 @@
 class TaskManager {
-  constructor(task) {
+  constructor(currentID = 0) {
     this.tasks = {
 
+    // End of this.tasks
     }
+    this.currentID = currentID;
+    
+
+
+  // End of constructor
   }
+
+  // name is the title of the description
+  addTask(name, description, assignedTo, dueDate, status = 'TODO') {
+    this.name = name;
+    this.description = description;
+    this.assignedTo = assignedTo;
+    this.dueDate = dueDate;
+    this.status = status;
+    this.currentID = this.currentID + 1;
+  }
+
+  addTask('john', 'wash red and blue clothing', 'do laundry', 
+  'jan 14')
+
+// End of TaskManager Class
 }
