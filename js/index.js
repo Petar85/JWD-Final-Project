b8d8649aa0a20ff65d99734ca97346d813cd8dda
Ben <92470875+BenjKa@users.noreply.github.com>
2021-12-07 10:31:33 -0800

    step 5 task 1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-let taskMan = new TaskManager();
-taskMan.addTask('name', 'description', 'assignedTo', 'dueDate', 'status');
-<<<<<<< HEAD
-console.log(taskMan.tasks);
+// let taskMan = new TaskManager();
+// taskMan.addTask('name', 'description', 'assignedTo', 'dueDate', 'status');
+// console.log(taskMan.tasks);
 
-=======
-console.log(taskMan.tasks);
->>>>>>> d05fad3367ef853a91a794c842c12846318d1b41
+
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,23 @@
+function createTaskHtml(name, description, assignedTo, dueDate, status) {
+  const htmlForTask = `  
+  <div class="row align-items-center">
+    <div class="col"> 
+      <h1 class="display-4"></h1>      
+      <div class="card mx-auto" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Task </h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card </h6>
+          <p class="card-text">${name} ${description} ${assignedTo} ${dueDate} ${status}</p>
+          <span class="badge badge-pill badge-success">Success</span>
+          <button type="button" class="btn btn-outline-primary">Primary</button>
+        </div>
+      </div> 
+    </div>
+  </div>
+  <br>`
+  return htmlForTask;
+}
+
 class TaskManager {
   constructor(currentID) {
     this.tasks = [];
@@ -23,6 +43,11 @@ class TaskManager {
 // End of TaskManager Class
 }
 
-let task1 = new TaskManager(1);
-task1.addTask('john', 'wash clothes', 'dwladjad', 'jan14');
-console.log(task1.tasks);
+// Task 4 Testing
+// let task1 = new TaskManager(1);
+// task1.addTask('john', 'wash clothes', 'dwladjad', 'jan14');
+// console.log(task1.tasks);
+
+//Task 5 Testing
+let taskHtml = createTaskHtml('John', 'laundry', 'unknown', 'wednesday', 'in progress');
+console.log(taskHtml);
