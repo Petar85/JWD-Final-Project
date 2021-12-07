29b07e3a0a8b274724e02841f2d95afa4cd78b21
Ben <92470875+BenjKa@users.noreply.github.com>
2021-12-07 15:28:49 -0800

    Fixed ID increments, render function.

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,5 +1,6 @@
+// This will create a task in HTML (Visually updates the website)
 function createTaskHtml(name, description, assignedTo, dueDate, status) {
-  const htmlForTask = `  
+  const html = `  
   <div class="row align-items-center">
     <div class="col"> 
       <h1 class="display-4"></h1>      
@@ -7,7 +8,7 @@ function createTaskHtml(name, description, assignedTo, dueDate, status) {
         <div class="card-body">
           <h5 class="card-title">Task </h5>
           <h6 class="card-subtitle mb-2 text-muted">Card </h6>
-          <p class="card-text">${name} ${description} ${assignedTo} ${dueDate} ${status}</p>
+          <p class="card-text">Name: ${name} \nDescription:${description} \nAssigned To:${assignedTo} \nDue Date:${dueDate} \nStatus: ${status}</p>
           <span class="badge badge-pill badge-success">Success</span>
           <button type="button" class="btn btn-outline-primary">Primary</button>
         </div>
@@ -15,7 +16,7 @@ function createTaskHtml(name, description, assignedTo, dueDate, status) {
     </div>
   </div>
   <br>`
-  return htmlForTask;
+  return html;
 // End of htmlForTask();
 }
 
@@ -28,7 +29,7 @@ class TaskManager {
   // End of constructor
   }
 
-  // name is the title of the description
+  // This will add a single task [to the list of tasks]
   addTask(name, description, assignedTo, dueDate) {
     const task = {
       name: name,
@@ -36,28 +37,32 @@ class TaskManager {
       assignedTo: assignedTo,
       dueDate: dueDate,
       status: 'TODO',
-      id: this.currentID + 1,
+      id: this.currentID,
     // End of task
     };
+    this.currentID++;
     this.tasks.push(task);
   // End of addTask();
   }
 
   render() {
     let tasksHtmlList = [];
-    for (let taskNumber = 0; taskNumber < TaskManager.tasks.length; taskNumber++) {
-      let currentTask = TaskManager.tasks[taskNumber];
+    for (let taskNumber = 0; taskNumber < this.tasks.length; taskNumber++) {
+      let currentTask = this.tasks[taskNumber];
       let date = new Date(currentTask.dueDate);
       let formattedDate = `Due date: ${date}`;
-      let taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo, formattedDate, currentTask.status);
+      let taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo, formattedDate, currentTask.status); 
       tasksHtmlList.push(taskHtml);
     }
     let tasksHtml = tasksHtmlList.join('\n');
-    document.getElementById("cardList").innerHTML(taskHtml);
+    document.getElementById("cardList").innerHTML(tasksHtml);
   }
 
 // End of TaskManager Class
 }
+
+
+// TESTING:
 
 // //Task 4 Testing
 // let task1 = new TaskManager(1);
@@ -65,13 +70,26 @@ class TaskManager {
 // console.log(task1.tasks);
 
 //Task 5 Testing
-let taskHtml = createTaskHtml('John', 'laundry', 'unknown', 'wednesday', 'in progress');
-console.log(taskHtml);
+// let taskHtml = createTaskHtml('John', 'laundry', 'unknown', 'wednesday', 'in progress');
+// console.log(taskHtml);
 
 let taskMan = new TaskManager(0);
-taskMan.addTask('TIm', 'Laundry', 'Himself', 'October 13', 'In progress');
-// console.log(taskMan.tasks);
+console.log("Length of the task: "  + taskMan.tasks.legnth);
+taskMan.addTask('Tim', 'Laundry', 'Himself', 'October 13', 'In progress');
+taskMan.addTask('John', 'laundry', 'unknown', 'wednesday', 'in progress');
+console.log(taskMan.tasks);
+console.log("Length of the task: "  + taskMan.tasks.legnth);
+taskMan.render();
+// taskMan.render();
 
-let submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("submit", addTask());
-TaskManager.render();
+// // grab user inputs
+// let name = document.querySelector('name');
+// let description = document.querySelector('description');
+// let assignedTo = document.querySelector('assignedTo');
+// let dueDate = document.querySelector('dueDate');
+// let button = document.querySelector('button');
+
+// // Submit Button
+// let submitButton = document.querySelector("submitButton");
+// submitButton.addEventListener("click", addTask(name, description, assignedTo, dueDate));
+// taskHtml.render();
