e282192b94eb3a6c59e72101e5c22ddb4e841260
Ben <92470875+BenjKa@users.noreply.github.com>
2021-12-10 14:13:37 -0800

    added done button, and made task card text look readable

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,16 +1,17 @@
-// This will create a task in HTML (Visually updates the website)
+// This will create a task in HTML and return it.
 function createTaskHtml(name, description, assignedTo, dueDate, status, id) {
-  const html = `  
-  <div class="row align-items-center">
+  const html =   
+  `<div class="row align-items-center">
     <div class="col"> 
       <h1 class="display-4"></h1>      
       <div class="card mx-auto" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Task ${id}</h5>
+          <h5 class="card-title">Task Number ${id}</h5>
           <h6 class="card-subtitle mb-2 text-muted">${name}</h6>
-          <p class="card-text">Description: ${description}\n Assigned To: ${assignedTo}</p>
+          <p class="card-text"><b>Description:</b> ${description}<br><b>Assigned To:</b> ${assignedTo}</p>
           <span class="badge badge-pill badge-success">${status}</span>
           <button type="button" class="btn btn-outline-primary">${dueDate}</button>
+          <button type="button" class="btn btn-success">Done</button>
         </div>
       </div> 
     </div>
@@ -20,6 +21,7 @@ function createTaskHtml(name, description, assignedTo, dueDate, status, id) {
 // End of htmlForTask();
 }
 
+// TaskManager class
 class TaskManager {
   constructor(currentID) {
     this.tasks = [];
@@ -30,7 +32,7 @@ class TaskManager {
   // End of constructor
   }
 
-  // This will add a single task [to the list of tasks]
+  // This will add a single task [to the list of tasks in the constructor]
   addTask(name, description, assignedTo, dueDate, status) {
     const task = {
       name: name,
@@ -48,7 +50,10 @@ class TaskManager {
     console.log(task);
   }
 
+  // The render() method takes the tasks from the TaskManager constructor,
+  // and it will display it to the browser.
   render() {
+
     let tasksHtmlList = [];
 
     for (let taskNumber = 0; taskNumber < this.tasks.length; taskNumber++) {
@@ -60,10 +65,8 @@ class TaskManager {
       let taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo, formattedDate, currentTask.status, taskNumber+1); 
       
       tasksHtmlList.push(taskHtml);
-      console.log(taskHtml);
     }
 
-    console.log(`This is the task HTML List ${tasksHtmlList}`);
     let tasksHtml = tasksHtmlList.join('\n');
     const cardText = document.getElementById("cardList");
     cardText.innerHTML = tasksHtml;
