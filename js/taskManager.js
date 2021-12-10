ca39006f7e7f469426ee6e3aa0494e83a8c473ea
Ben <92470875+BenjKa@users.noreply.github.com>
2021-12-10 14:42:33 -0800

    added comments

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,5 +1,7 @@
 // This will create a task in HTML and return it.
 function createTaskHtml(name, description, assignedTo, dueDate, status, id) {
+  
+  // Template to create a single task in HTML code.
   const html =   
   `<div class="row align-items-center">
     <div class="col"> 
@@ -17,22 +19,27 @@ function createTaskHtml(name, description, assignedTo, dueDate, status, id) {
     </div>
   </div>
   <br>`
+
+  // The function createTaskHtml() will return a string of a single 
+  // task in HTML code.
   return html;
-// End of htmlForTask();
+
+//End of createTaskHtml();
 }
+
 
 // TaskManager class
 class TaskManager {
+
   // The constructor: holds information that the TaskManager needs.
   constructor(currentID) {
     // This will contain ALL of the tasks
     this.tasks = [];
-    
     // The id will identify each task to a number.
     this.currentID = 0;
     this.currentID = currentID;
     
-  // End of constructor
+  //End of constructor
   }
 
 
@@ -48,41 +55,54 @@ class TaskManager {
       status: status,
       id: this.currentID,
 
-    // End of task
+    //End of addTask
     };
 
-    // Increment the ID (So that the next task will not have
+    // Increment the ID (so that the next task will not have
     // the same ID as the previous task)
     this.currentID++;
 
     // Push the task to the list of tasks in the constructor
     this.tasks.push(task);
 
-  // End of addTask();
+  //End of addTask();
   }
 
   // The render() method takes the tasks from the TaskManager constructor,
   // and it will display it to the browser.
   render() {
 
+    // Variable that contains a list of tasks in HTML format.
     let tasksHtmlList = [];
 
+    // Loop to iterate over EVERY task that has been created so far.
     for (let taskNumber = 0; taskNumber < this.tasks.length; taskNumber++) {
       let currentTask = this.tasks[taskNumber];
-      // Code below did not work, sets year to 2001
+      
+      //BUG: Code below did not work, it sets the year to 2001
       // let date = new Date(currentTask.dueDate);
+
+      // Code to format the date (to be readable in the task cards)
       let date = currentTask.dueDate;
       let formattedDate = `Due date: ${date}`;
+
+      // Will grab a task, and turn it into HTML format.
       let taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo, formattedDate, currentTask.status, taskNumber+1); 
       
+      // Pushes the HTML formatted task to tasksHtmlList
       tasksHtmlList.push(taskHtml);
     }
 
+    // Joins the array, so that all the items in the array
+    // will combine and convert into a single string
     let tasksHtml = tasksHtmlList.join('\n');
+
+    // Replaces the code in <div id="cardList"></div> from the
+    // index.html file, to contain the proper task cards.
     const cardText = document.getElementById("cardList");
     cardText.innerHTML = tasksHtml;
     
   }
 
-// End of TaskManager Class
+//End of TaskManager Class
 }
