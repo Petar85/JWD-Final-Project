f47abefc15559f12e182855d9537147f3a2610a7
Ben <92470875+BenjKa@users.noreply.github.com>
2021-12-10 14:29:07 -0800

    added comments to make code more understandable

--- js/index.js
+++ js/index.js
@@ -1,49 +1,34 @@
-// let taskMan = new TaskManager();
-// taskMan.addTask('Tim', 'Laundry', 'Himself', 'October 13', 'In progress');
-// console.log(taskMan.tasks);
-
-// let submitButton = document.getElementById("submitButton");
-// subtmitButton.addEventListener("submit", addTask());
-// TaskManager.render();
-
-//Task 5 Testing
-  // Creates a task html and console.logs it
-// let taskHtml = createTaskHtml('John', 'laundry', 'unknown', 'wednesday', 'in progress');
-// console.log(`Prints task HTML: ${taskHtml}`);
-
-// Hard coded tasks in
-// Creates a new TaskManager class, that includes a list of tasks
+// Creating a variable of the TaskManager class
 let taskMan = new TaskManager(0);
-// //0 Tasks because no tasks were added yet
-// console.log("Length of the task: "  + taskMan.tasks.legnth);
+
+
+// Hard coded tasks
 taskMan.addTask('Dusting', 'Dust off the desk.', 'Lisa', 'Dec 13', 'In progress');
 taskMan.addTask('Laundry', 'Fold the clean clothes.', 'John', 'Dec 14', 'In progress');
 taskMan.addTask('Dishes', 'Load the dishwasher.', 'Kate', 'Dec 15', 'In progress');
 taskMan.addTask('Floor', 'Mop and vacum the floors.', 'Bobby', 'Dec 16', 'In progress');
 taskMan.addTask('Generation', 'Graduate the Generation class.', 'Students', 'Dec 17', 'In progress');
-// // 5 Tasks because 5 tasks were added
-// console.log(taskMan.tasks);
 
-// console.log("Length of the task: "  + taskMan.tasks.legnth);
-// taskMan.render();
 
-// const taskForm = document.getElementById("cardList");
-// const submitButton2 = document.getElementById("submitButton2");
-// const theStartButton = document.getElementById("TheStartButton");
-
-// // Submit Button
+// Getting the id of the submit button
 let submitButton = document.getElementById("submitButton");
 
+
+// Code to execute when the submit button is clicked
 submitButton.addEventListener("click", (event) => {
+
+    // Prevents the webpage from reloading (loses all tasks)
     event.preventDefault();
-    // // Grabs the user's inputs from the form
+
+
+    // Grabs the user's inputs from the form
     let name = document.getElementById('name').value;
     let description = document.getElementById('description').value;
     let assignedTo = document.getElementById('assignedTo').value;
     let dueDate = document.getElementById('dueDate').value;
 
     
-
+    // Form validation
     if (name == '') {
         alert("Please put a name");
     } else if(description == '') {
@@ -53,9 +38,11 @@ submitButton.addEventListener("click", (event) => {
     } else if(dueDate == '') {
         alert("Please put the task's due date");
     } else {
-        taskMan.render();
+        // If the form was filled out correctly (No alerts), 
+        // Add the task the user submitted
         taskMan.addTask(name, description, assignedTo, dueDate, "In Progress");
+        // Then render the page (Be able to display the tasks at the bottom)
+        taskMan.render();
     }
     
 });
-console.log(`Current list of tasks: ${taskMan.tasks}`);
